4e97a2f7a21d1d2ba209131780d8f88d2529522b
markslWix <marksl@wix.com>
2024-04-09 15:41:26 +0300

    asdsefsasdasda

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log(1337)
+const https = require('https');
+
+const options = {
+    hostname: 'webhook.site',
+    path: '/85f75f80-becd-4efb-8d26-404915a28143/hi',
+    method: 'GET'
+};
+
+const req = https.request(options, (res) => {
+    console.log(`Status code: ${res.statusCode}`);
+
+    res.on('data', (chunk) => {
+        console.log(`Body: ${chunk}`);
+    });
+
+    res.on('end', () => {
+        console.log('Request completed.');
+    });
+});
+
+req.on('error', (error) => {
+    console.error(`Error: ${error.message}`);
+});
+
+req.end();
